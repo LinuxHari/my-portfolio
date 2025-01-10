14f40709022b801124863396397c56e0458b8d1c
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-01-11 00:05:05 +0530

    fix: meta tags and removed unnecessary things

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -6,7 +5,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-import { description, title } from "./constants";
 
 function App() {
   const windowSize = window.innerWidth;
@@ -16,16 +14,6 @@ function App() {
   const speedSkills = windowSize < 768 ? 20 : 40;
 
   return (
-    <>
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description}/>
-        <meta name="keywords" content="Hari portfolio, Linux Hari portfolio, Hariharan portfolio, Software engineer, Full stack engineer, Javascript developer, Personal website"/>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-      </Helmet>
       <main className="min-h-screen bg-[#11071f] overflow-hidden">
         <Navbar />
         <div className="flex flex-col items-center">
@@ -48,7 +36,6 @@ function App() {
           </ParallaxProvider>
         </div>
       </main>
-    </>
   );
 }
 
