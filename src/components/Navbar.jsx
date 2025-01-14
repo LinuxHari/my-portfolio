520199d5b35be490c23b94bed802491bbdaf26a8
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-01-14 19:00:33 +0530

    fix: nav issue fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { NAV_LINKS } from "../data";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 
@@ -27,7 +26,11 @@ const Navbar = () => {
        <NavLinks className="static bg-none top-0 hidden sm:inline-block"/>
       </nav>
     </header>
-    <NavLinks className={`fixed ${showBottomNav ? "top-[95vh] left-1/2 -translate-x-1/2": "top-[17px] right-5"} sm:hidden duration-200 transform z-max bg-[#11071f]`}/>
+    <NavLinks
+        className={`fixed sm:hidden z-max bg-[#11071f] 
+          ${showBottomNav ? "left-1/2 -translate-x-1/2 bottom-0" : "top-[17px] right-5"}
+          duration-0`}
+      />
    </>
   );
 };
