2b7e4907034da914e04354c67843cbd68808f8fe
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-05-11 14:57:59 +0530

    fix: commented import

--- app/components/Hero/Showreel.tsx
+++ app/components/Hero/Showreel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useGSAP } from "@gsap/react";
-import { gsap } from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import { gsap } from "gsap";
 import { useRef } from "react";
 
 const Showreel = () => {
